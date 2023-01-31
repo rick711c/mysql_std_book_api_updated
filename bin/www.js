2f1c0456ba0cb4fbd602898ee8c2a88fe51c5638
rick711c <67436517+rick711c@users.noreply.github.com>
2023-01-31 17:38:35 +0530

    third commit

--- bin/www.js
+++ bin/www.js
@@ -1,7 +1,2 @@
-const express=require('express');
-const app=express();
-//const std=require("../controller/std_logic")
-const UseRouter=require("../controller/api/router")
-app.use(express.json());
-app.use("/ping",UseRouter)
+const app=require("../app")
 app.listen(3000);
